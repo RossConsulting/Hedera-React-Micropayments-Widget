0299c3f01f910e56b4bceda0dfd09d326d6829de
Dawid Loubser <dawid.loubser@ibi.co.za>
2019-07-31 23:36:30 +0200

    Removing files to restructure for tooling change (nwb)

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import HederaMicropayment from './HederaMicropayment';
-
-ReactDOM.render(<HederaMicropayment maximumAmount="100000">
-  <App/>
-</HederaMicropayment>, document.getElementById('root'));
